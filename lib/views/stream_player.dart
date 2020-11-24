c476a4128493e2e640e6e6196287d8412e084103
immadisairaj <immadirajendra.sai@gmail.com>
2020-11-24 11:11:16 +0530

    replace draggablescrollsheet with slidinguppanel

--- lib/views/stream_player.dart
+++ lib/views/stream_player.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_radio_player/flutter_radio_player.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:radiosai/constants/constants.dart';
 import 'package:radiosai/views/stream_select.dart';
 
@@ -23,6 +24,8 @@ class _StreamPlayer extends State<StreamPlayer> with SingleTickerProviderStateMi
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   Future<int> _streamIndex;
 
+  PanelController _panelController = new PanelController();
+
   FlutterRadioPlayer _flutterRadioPlayer = new FlutterRadioPlayer();
 
   @override
@@ -31,6 +34,16 @@ class _StreamPlayer extends State<StreamPlayer> with SingleTickerProviderStateMi
     super.initState();
     _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 300));
     _streamIndex = _prefs.then((SharedPreferences prefs) {return (prefs.getInt('stream') ?? 0);});
+  }
+
+  Future<void> updateStreamIndex() async {
+    setState(() {
+      _streamIndex = _prefs.then((SharedPreferences prefs) {return (prefs.getInt('stream') ?? 0);});
+      if(isPlaying == true) {
+        isPlaying = !isPlaying;
+        _animationController.reverse().then((value) => stopRadioService());
+      }
+    });
   }
 
   Future<void> initRadioService(int index) async {
@@ -65,92 +78,137 @@ class _StreamPlayer extends State<StreamPlayer> with SingleTickerProviderStateMi
 
   @override
   Widget build(BuildContext context) {
+    BorderRadiusGeometry radius = BorderRadius.only(
+      topLeft: Radius.circular(24.0),
+      topRight: Radius.circular(24.0),
+    );
     return Scaffold(
-      body: Stack(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Image(
-              fit: BoxFit.fitHeight,
-              image: AssetImage('assets/sai_listens.jpg'),
+      body: SlidingUpPanel(
+        borderRadius: radius,
+        backdropEnabled: true,
+        controller: _panelController,
+        onPanelClosed: () {
+          setState(() async {
+            await _flutterRadioPlayer.isPlaying()
+            .then((value) {
+              if(!value) {
+                updateStreamIndex();
+              }
+            });
+          });
+        },
+        collapsed: GestureDetector(
+          onTap: () {
+            _panelController.open();
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: radius,
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 12),
+                Container(
+                  height: 5,
+                  width: 30,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                SizedBox(height: 12),
+                Text(
+                  'Select Stream',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 12),
+              ],
             ),
           ),
-          Center(
-            child: FutureBuilder<int>(
-              future: _streamIndex,
-              builder: (BuildContext context, AsyncSnapshot<int> snapshotInt) {
-                switch(snapshotInt.connectionState) {
-                  case ConnectionState.waiting:
-                    return const CircularProgressIndicator();
-                  default:
-                    if(snapshotInt.hasError) {
-                      return Text('Error');
-                    } else {
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            MyConstants.of(context).streamName[snapshotInt.data],
-                          ),
-                          Opacity(
-                            opacity: 0.7,
-                            child: IconButton(
-                              iconSize: 90,
-                              color: Colors.white,
-                              icon: AnimatedIcon(
-                                icon: AnimatedIcons.play_pause,
-                                progress: _animationController,
-                              ),
-                              onPressed: () async {
-                                _handleOnPressed(snapshotInt.data);
+        ),
+        panel: StreamList(
+          flutterRadioPlayer: _flutterRadioPlayer,
+          panelController: _panelController,
+          animationController: _animationController,
+        ),
+        body: Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Image(
+                fit: BoxFit.fitHeight,
+                image: AssetImage('assets/sai_listens.jpg'),
+              ),
+            ),
+            Center(
+              child: FutureBuilder<int>(
+                future: _streamIndex,
+                builder: (BuildContext context, AsyncSnapshot<int> snapshotInt) {
+                  switch(snapshotInt.connectionState) {
+                    case ConnectionState.waiting:
+                      return const CircularProgressIndicator();
+                    default:
+                      if(snapshotInt.hasError) {
+                        return Text('Error');
+                      } else {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              MyConstants.of(context).streamName[snapshotInt.data],
+                            ),
+                            Opacity(
+                              opacity: 0.7,
+                              child: IconButton(
+                                iconSize: 90,
+                                color: Colors.white,
+                                icon: AnimatedIcon(
+                                  icon: AnimatedIcons.play_pause,
+                                  progress: _animationController,
+                                ),
+                                onPressed: () async {
+                                  _handleOnPressed(snapshotInt.data);
+                                },
+                              )
+                            ),
+                            StreamBuilder(
+                              stream: _flutterRadioPlayer.isPlayingStream,
+                              initialData: widget.playerState,
+                              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                                String returnData = snapshot.data;
+                                print("object data: " + returnData);
+                                switch(returnData) {
+                                  case FlutterRadioPlayer.flutter_radio_paused:
+                                    _flutterRadioPlayer.play();
+                                    return Text('Loading stream..'); // TODO: add loading widget
+                                  case FlutterRadioPlayer.flutter_radio_stopped:
+                                    return Text('Play');
+                                    break;
+                                  case FlutterRadioPlayer.flutter_radio_loading:
+                                  // TODO: add loading widget
+                                    return Text("Loading stream..");
+                                  case FlutterRadioPlayer.flutter_radio_error:
+                                  // doesn't handle error state
+                                    // TODO: add notify to retry or check internet or so
+                                    return Text('Retry');
+                                    break;
+                                  default:
+                                    return Text('Playing');
+                                }
                               },
-                            )
-                          ),
-                          StreamBuilder(
-                            stream: _flutterRadioPlayer.isPlayingStream,
-                            initialData: widget.playerState,
-                            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                              String returnData = snapshot.data;
-                              print("object data: " + returnData);
-                              switch(returnData) {
-                                case FlutterRadioPlayer.flutter_radio_paused:
-                                  _flutterRadioPlayer.play();
-                                  return Text('Loading stream..'); // TODO: add loading widget
-                                case FlutterRadioPlayer.flutter_radio_stopped:
-                                  return Text('Play');
-                                  break;
-                                case FlutterRadioPlayer.flutter_radio_loading:
-                                // TODO: add loading widget
-                                  return Text("Loading stream..");
-                                case FlutterRadioPlayer.flutter_radio_error:
-                                // doesn't handle error state
-                                  // TODO: add notify to retry or check internet or so
-                                  return Text('Retry');
-                                  break;
-                                default:
-                                  return Text('Playing');
-                              }
-                            },
-                          ),
-                        ],
-                      );
-                    }
-                }
-              },
+                            ),
+                          ],
+                        );
+                      }
+                  }
+                },
+              ),
             ),
-          ),
-          DraggableScrollableSheet(
-            initialChildSize: 0.12,
-            minChildSize: 0.12,
-            maxChildSize: 0.67,
-            builder: (context, scrollController) {
-              return SingleChildScrollView(
-                controller: scrollController,
-                child: StreamSelect(_flutterRadioPlayer),
-              );
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/views/stream_select.dart
+++ lib/views/stream_select.dart
@@ -1,63 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_radio_player/flutter_radio_player.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:radiosai/views/stream_player.dart';
 import 'package:radiosai/constants/constants.dart';
 
-class StreamSelect extends StatelessWidget {
-  StreamSelect(this.flutterRadioPlayer);
-  FlutterRadioPlayer flutterRadioPlayer;
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 12.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(24),
-      ),
-      margin: const EdgeInsets.all(0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: 12),
-            DraggingHandle(),
-            SizedBox(height: 8),
-            Text(
-              'Select Stream',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            StreamList(flutterRadioPlayer: flutterRadioPlayer),
-            SizedBox(height: 12),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DraggingHandle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 5,
-      width: 30,
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(16),
-      ),
-    );
-  }
-}
-
 class StreamList extends StatefulWidget {
-  StreamList({Key key, this.flutterRadioPlayer}) : super(key: key);
+  StreamList({Key key, this.flutterRadioPlayer,
+              this.panelController,
+              this.animationController,}) : super(key: key);
 
   FlutterRadioPlayer flutterRadioPlayer;
+  PanelController panelController;
+  AnimationController animationController;
 
   @override
   _StreamList createState() => _StreamList(); 
@@ -79,36 +34,38 @@ class _StreamList extends State<StreamList> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 5/4,
-      ),
-      itemCount: MyConstants.of(context).streamName.length,
-      primary: false,
-      shrinkWrap: true,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.all(8),
-          child: GestureDetector(
-            onTap: () async {
-            // TODO: add the stream
-            setStream(index);
-            // TODO: Have to replace with smooth transition
-            widget.flutterRadioPlayer.stop();
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => StreamPlayer(),
-              )
-            );
-          },
-            child: Card(
-                child: Text(MyConstants.of(context).streamName[index]),
+    return Padding(
+      padding: const EdgeInsets.only(top: 25),
+      child: GridView.builder(
+        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 5/4,
+        ),
+        itemCount: MyConstants.of(context).streamName.length,
+        primary: false,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.all(8),
+            child: GestureDetector(
+              onTap: () async {
+              setStream(index);
+              await widget.flutterRadioPlayer.isPlaying()
+              .then((value) {
+                widget.flutterRadioPlayer.stop();
+              })
+              .then((value) => widget.panelController.close());
+            },
+              child: Card(
+                elevation: 1.8,
+                child: Center(
+                  child: Text(MyConstants.of(context).streamName[index]),
+                ),
               ),
             ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
