ebeed83cb8d58bf9ff2ddaf93452f9e540360676
immadisairaj <immadirajendra.sai@gmail.com>
2021-04-17 11:21:20 +0530

    add date picker

--- lib/screens/sai_inspires/sai_inspires.dart
+++ lib/screens/sai_inspires/sai_inspires.dart
@@ -22,30 +22,38 @@ class _SaiInspires extends State<SaiInspires> {
   // final String baseUrl = 'https://www.radiosai.org/pages/calthought2.asp';
   final String baseUrl = 'https://www.radiosai.org/pages/ThoughtText.asp';
 
+  final DateTime now = DateTime.now();
+  DateTime selectedDate;
+  String imageFinalUrl;
+  String finalUrl;
+
   @override
   void initState() {
+
+    selectedDate = now;
+    _updateURL(selectedDate);
+
     super.initState();
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
   @override
   Widget build(BuildContext context) {
-    DateTime now = DateTime.now();
-    // String formattedDate = DateFormat('yyyyMMdd').format(now);
-    String imageFormattedDate = DateFormat('yyyyMMdd').format(now);
-    String formattedDate = DateFormat('MM/dd/yyyy').format(now);
-    // String finalUrl = '$baseUrl/${now.year}/SI_$formattedDate.htm';
-    String imageFinalUrl = '$imageBaseUrl/${now.year}/uploadimages/SI_$imageFormattedDate.jpg';
-    String finalUrl = '$baseUrl?mydate=$formattedDate';
     return Scaffold(
       appBar: AppBar(
         title: Text('Sai Inspires'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.date_range_outlined),
+            onPressed: () => _selectDate(context),
+          ),
+        ],
       ),
-      // TODO: add zoom effect or change link
       body: Container(
         color: Colors.white,
         child: Padding(
           padding: const EdgeInsets.only(top: 10),
+          // TODO: add loading progress
           child: Column(
             children: [
               Padding(
@@ -55,12 +63,15 @@ class _SaiInspires extends State<SaiInspires> {
                   child: Image.network(imageFinalUrl),
                 ),
               ),
+              // TODO: try changing WevView to InAppWebView for larger text
               Expanded(
                 child: WebView(
                   initialUrl: finalUrl,
                   javascriptMode: JavascriptMode.unrestricted,
-                  onPageFinished: (url) {
-                    _webViewController.evaluateJavascript('document.body.style.zoom = 0.7');
+                  onPageFinished: (url) async {
+                    _webViewController.evaluateJavascript(
+                      "document.body.style.zoom = 0.7; document.getElementById('Home').remove(); document.getElementById('Official').remove()"
+                    );
                   },
                   onWebViewCreated: (controller) {
                     _webViewController = controller;
@@ -73,4 +84,31 @@ class _SaiInspires extends State<SaiInspires> {
       ),
     );
   }
+
+  _updateURL(DateTime date) async {
+    // String formattedDate = DateFormat('yyyyMMdd').format(now);
+    String imageFormattedDate = DateFormat('yyyyMMdd').format(date);
+    String formattedDate = DateFormat('dd/MM/yyyy').format(date);
+    // String finalUrl = '$baseUrl/${now.year}/SI_$formattedDate.htm';
+    imageFinalUrl = '$imageBaseUrl/${date.year}/uploadimages/SI_$imageFormattedDate.jpg';
+    finalUrl = '$baseUrl?mydate=$formattedDate';
+    if(_webViewController != null) await _webViewController.loadUrl(finalUrl);
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      // Sai Inspires started on 1st May 2010
+      firstDate: DateTime(2011, 2, 19),
+      initialDate: selectedDate,
+      lastDate: now,
+    );
+    if(picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+        _updateURL(selectedDate);
+      });
+    }
+  }
+
 }
