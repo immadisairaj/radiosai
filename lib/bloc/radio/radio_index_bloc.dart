5237fac14dd8874b2870baca26712bd96d560e8c
immadisairaj <immadirajendra.sai@gmail.com>
2021-05-04 10:56:44 +0530

    fix shared prefs update when starting radio change
    When the starting radio stream is changed, the value updates in shared
    prefs of current radio stream also (on app start)

--- lib/bloc/radio/radio_index_bloc.dart
+++ lib/bloc/radio/radio_index_bloc.dart
@@ -38,6 +38,8 @@ class RadioIndexBloc {
 
       _actionController.stream.listen(_changeStream);
       _addValue.add(_index);
+      // add in shared prefences as to not when opened next time
+      value.setInt(_indexKey, _index);
     });
   }
 
